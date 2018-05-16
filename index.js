7e975214f572c3298ef6f3359bd36254b2959bdc
Sheena B <sheena.myb@gmail.com>
2018-05-16 07:33:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -37,6 +37,5 @@ function prependKitten() {
 
 // Remove last kitten in array and return new array, aka "Non-mutated"
 function removeLastKitten() {
-  var byeByeKitty = kittens.slice(2);
-  return byeByeKitty;
+  cons 
 }
