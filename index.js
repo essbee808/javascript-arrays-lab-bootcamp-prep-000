87967c928f2d1cf50e9b03f8b474650717caac20
Sheena B <sheena.myb@gmail.com>
2018-05-16 07:15:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var kittens = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendKitten() {
-  var kittens = [...kittens, "Meeko"];
+  var kittens = [...kittens, "Ralph"];
   return kittens;
 }
